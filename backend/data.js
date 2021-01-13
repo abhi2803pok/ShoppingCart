2a0fe21b89e1f0158d52d3a4f6dea08c55c7a73e
abhi2803pok <pokalaabhinav@gmail.com>
2021-01-12 22:20:26 -0500

    sec

---
+++ backend/data.js
@@ -0,0 +1,64 @@
+// eslint-disable-next-line no-undef
+// eslint-disable-next-line import/no-anonymous-default-export
+export default  {
+    products:[{
+        _id:'1',
+        name:'slim sports shirt',
+        category:"shirts",
+        image:'/Images/Jersey_IND_SLIM.jpg',
+        price:60 ,
+        brand:'NIKE',
+        rating:4.5,
+        reviews:4.5
+    },
+    {
+        _id:'2',
+        name:'slim sports shirt',
+        category:"shirts",
+        image:'/Images/Jersey_IND_SLIM.jpg',
+        price:60,
+        brand:'NIKE',
+        rating:4.5,
+        reviews:4.5
+    },
+    {
+        _id:'3',
+        name:'slim sports shirt',
+        category:"shirts",
+        image:'/Images/Jersey_IND_SLIM.jpg',
+        price:60,
+        brand:'NIKE',
+        rating:4.5,
+        reviews:4.5
+    },
+    {
+        _id:'4',
+        name:'slim sports shirt',
+        category:"shirts",
+        image:'/Images/Jersey_IND_SLIM.jpg',
+        price:60,
+        brand:'NIKE',
+        rating:5,
+        reviews:4.5
+    },
+    {
+        _id:'5',
+        name:'Best pants',
+        category:"Pants",
+        image:'/Images/Jersey_IND_SLIM.jpg',
+        price:45,
+        brand:'NIKE',
+        rating:4.2,
+        reviews:4.2
+    },
+    {
+        _id:'6',
+        name:'fit sports shirt',
+        category:"shirts",
+        image:'/Images/Jersey_IND_SLIM.jpg',
+        price:30,
+        brand:'NIKE',
+        rating:4,
+        reviews:4.5
+    }]
+}
